51a5a464f2acdc1a3104ec64e5bfbd0cd89963a3
jai lalawat <jailalawat@gmail.com>
2019-09-06 19:26:36 +0530

    update other pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # root "application#index"
-  # get 'web-application-development' => 'application#webDesign', as: :web_design
-  # get 'internet-of-things' => 'application#internet', as: :internet
-  # get 'digital-strategy-consulting' => 'application#digital', as: :digital
-  # get 'big-data-analytics' => 'application#bigData', as: :big_data
+  get 'web-application-development' => 'application#webDesign', as: :web_design
+  get 'internet-of-things' => 'application#internet', as: :internet
+  get 'digital-strategy-consulting' => 'application#digital', as: :digital
+  get 'big-data-analytics' => 'application#bigData', as: :big_data
 
-  # post 'contact_message' => 'application#contact_message'
 
-  # root "welcome#index"
 
   get 'about' => 'application#about'
   get 'services' => 'application#services'
